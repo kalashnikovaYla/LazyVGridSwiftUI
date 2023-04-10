0d07d0a6cf8a74daccefcd171873493c1952a2c9
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-10 20:45:05 +0500

    Initial commit

--- LazyVGridSwiftUI/ContentView.swift
+++ LazyVGridSwiftUI/ContentView.swift
@@ -8,14 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns: [GridItem] = [.init(.flexible()),
+                               .init(.flexible()),
+                               .init(.flexible())
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ScrollView {
+            LazyVGrid(columns: columns,
+                      alignment: .center,
+                      spacing: nil,
+                      pinnedViews: [.sectionHeaders]) {
+                profileSection
+                photoSection
+            }
+        }
+    }
+    
+    var profileSection: some View {
+        Section {
+            VStack {
+                Image("image1")
+                    .resizable()
+                    .frame(width: 55, height: 55)
+                    .clipShape(Circle())
+                
+                Text("Converse")
+                    .fontWeight(.bold)
+            }
+            VStack {
+                Text("150")
+                    .fontWeight(.bold)
+                Text("Subscribe")
+                    .fontWeight(.bold)
+            }
+            VStack {
+                Text("120")
+                    .fontWeight(.bold)
+                Text("Subscriber")
+                    .fontWeight(.bold)
+            }
+            
+        } header: {
+            HStack {
+                Spacer()
+                Text("Shoes")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+            }
         }
         .padding()
+        .background(.white)
+    }
+    
+    var photoSection: some View {
+        Section {
+            ForEach(0..<50) { index in
+                Image("image1")
+                    .resizable()
+                    .frame(height: 150)
+            }
+        } header: {
+            
+            HStack {
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "grid")
+                }
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "globe")
+                }
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "video")
+                }
+            }
+            .padding()
+            .background(.white)
+        }
+
     }
 }
 
