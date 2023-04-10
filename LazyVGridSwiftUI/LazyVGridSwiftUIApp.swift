fe658c6ef52f6b663048070b069fd942207b0ca1
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-10 09:13:58 +0500

    Initial Commit

---
+++ LazyVGridSwiftUI/LazyVGridSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridSwiftUIApp.swift
+//  LazyVGridSwiftUI
+//
+//  Created by sss on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
